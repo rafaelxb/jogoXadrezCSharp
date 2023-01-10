80abaf6708b96cf8de0d1279ec816bcb4ad1b8da
rafaelxb <rfxbarbosa@gmail.com>
2023-01-09 21:47:33 -0300

    Tratando erros com exception personalizada

--- XadrezConsoleApplication/tabuleiro/Tabuleiro.cs
+++ XadrezConsoleApplication/tabuleiro/Tabuleiro.cs
@@ -29,8 +29,18 @@ namespace tabuleiro
             return pecas[pos.linha,pos.coluna];
         }
 
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
